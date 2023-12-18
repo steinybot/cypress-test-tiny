1c1f8a03fb2dc5d337281f8e06c2532c32cc6fe6
Jason Pickens <jasonpickensnz@gmail.com>
2023-12-18 20:33:40 +1300

    Setup

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,38 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+    it('works', () => {
+        const makeRequest = true;
+
+        cy.intercept(
+            {
+                method: "POST",
+                url: "/route",
+            },
+            cy.spy().as("request")
+        ).as("intercept");
+
+        const click = () => {
+            if (makeRequest) {
+                fetch(
+                    "/route",
+                    {
+                        method: "POST",
+                        body: "{'a': 1}",
+                    });
+            }
+        };
+
+        cy.get('body').then((body) => {
+            const button = document.createElement("button")
+            button.id = "test-button"
+            button.textContent = "Test"
+            button.addEventListener("click", click)
+            body[0].appendChild(button)
+        })
+
+        cy.get('#test-button').click()
+
+        // cy.get("@request").should("have.been.called");
+        cy.get("@request", { timeout: 0 }).should("not.have.been.called");
+    })
 })
